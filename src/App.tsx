ec7cc518a06c16d8942a1e43379418c8b7413afd
Kirill <kirypleshkov@gmail.com>
2023-08-30 22:54:24 +0700

    v1.1 started routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Qwer from './pages/Qwer';
 
 function App() {
+
   return (
-    <>
-      frer
-    </>
+    <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/qwer" element={<Qwer/>} />
+       </Routes>
+     </BrowserRouter>
   );
 }
 
---
+++ src/pages/Qwer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface QwerProps {
+    
+}
+
+const Qwer: React.FC<QwerProps> = () => {
+    return (
+        <div>
+            Qwer
+        </div>
+    );
+};
+
+export default Qwer;
